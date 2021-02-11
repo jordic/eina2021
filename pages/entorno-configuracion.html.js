cd854792305351c64e17e703387711d53e1c8a7b
Jordi Collell <jordic@gmail.com>
2021-02-11 05:43:59 +0100

    fix https youtube

--- pages/entorno-configuracion.html.js
+++ pages/entorno-configuracion.html.js
@@ -110,7 +110,7 @@ para localizar el servidor donde tenemos alojado nuestra web.
 function Youtube({id, height=600}) {
   return (
     <iframe id="player" type="text/html" width="100%" height={height}
-      src={'http://www.youtube.com/embed/' + id + '?cc_load_policy=3&?rel=0'}
+      src={'https://www.youtube.com/embed/' + id + '?cc_load_policy=3&?rel=0'}
       frameborder="0"></iframe>
   )
 }
